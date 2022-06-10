916b8d2d721afd33241a7ef30b639b7bc014c129
Rian Marlon Beckert <81371744+RianTMJ@users.noreply.github.com>
2022-06-10 14:53:31 -0300

    Update settings.py

--- fusion/settings.py
+++ fusion/settings.py
@@ -131,15 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email teste console
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-"""
-# Email produção
-
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = 'no-reply@fusion.com.br'
-EMAIL_PORT = 587
-EMAIL_USE_TSL = True
-EMAIL_HOST_PASSWORD = 'fusion'
-DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
-"""
 
 LOGOUT_REDIRECT_URL = 'index'
